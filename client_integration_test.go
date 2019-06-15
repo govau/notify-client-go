e79095b7a91d275a7390bc4a13755e30a069f327
Andrew Oh <andrewoh531@gmail.com>
2019-06-15 10:17:01 +1000

    Comments

--- client_integration_test.go
+++ client_integration_test.go
@@ -7,43 +7,42 @@ import (
 	notify "github.com/govau/notify-client-go"
 )
 
-func setup(t *testing.T) (*notify.Client, string, string) {
+func setup(t *testing.T) (client *notify.Client, smsTemplateID string, emailTemplateID string) {
 	apiKey := os.Getenv("API_KEY")
 	if apiKey == "" {
-		t.Fatal("API_KEY environment variable not set")
+		t.Fatal("could not set API_KEY environment variable")
 	}
 
-	smsTemplateId := os.Getenv("SMS_TEMPLATE_ID")
-	if smsTemplateId == "" {
-		t.Fatal("SMS_TEMPLATE_ID environment variable not set")
+	smsTemplateID = os.Getenv("SMS_TEMPLATE_ID")
+	if smsTemplateID == "" {
+		t.Fatal("could not set SMS_TEMPLATE_ID environment variable")
 	}
 
-	emailTemplateId := os.Getenv("EMAIL_TEMPLATE_ID")
-	if emailTemplateId == "" {
-		t.Fatal("EMAIL_TEMPLATE_ID environment variable not set")
+	emailTemplateID = os.Getenv("EMAIL_TEMPLATE_ID")
+	if emailTemplateID == "" {
+		t.Fatal("could not set EMAIL_TEMPLATE_ID environment variable")
 	}
 
 	var err error
-	client, err := notify.NewClient(
+	client, err = notify.NewClient(
 		apiKey,
 	)
 	if err != nil {
-		t.Fatal("Error creating client", err)
+		t.Fatal("could not create Notify client", err)
 	}
-
-	return client, smsTemplateId, emailTemplateId
+	return
 }
 
 func TestSendSMS(t *testing.T) {
-	client, smsTemplateId, _ := setup(t)
+	client, smsTemplateID, _ := setup(t)
 	phoneNumber := os.Getenv("SMS_RECIPIENT_NUMBER")
 	if phoneNumber == "" {
-		t.Fatal("SMS_RECIPIENT_NUMBER environment variable not set")
+		t.Fatal("could not set SMS_RECIPIENT_NUMBER environment variable")
 	}
 
 	ref := "TestSendSMS"
 	resp, err := client.SendSMS(
-		smsTemplateId,
+		smsTemplateID,
 		phoneNumber,
 		notify.Personalisation{
 			{"name", "John"},
@@ -52,7 +51,7 @@ func TestSendSMS(t *testing.T) {
 		notify.Reference(ref),
 	)
 	if err != nil {
-		t.Fatalf("Error sending SMS: %v", err)
+		t.Errorf("could not send SMS: %v", err)
 	}
 
 	if resp.ID == "" {
@@ -61,12 +60,17 @@ func TestSendSMS(t *testing.T) {
 	if resp.URI == "" {
 		t.Errorf("Response URI should not be empty")
 	}
-	assertEqual(t, ref, *resp.Reference)
-	assertEqual(t, "Hello John,\n\nToday is Friday.", resp.Content.Body)
+	if *resp.Reference != ref {
+		t.Errorf("Got %v, want %v", *resp.Reference, ref)
+	}
+	wantedBody := "Hello John,\n\nToday is Friday."
+	if resp.Content.Body != wantedBody {
+		t.Errorf("Got %v, want %v", resp.Content.Body, wantedBody)
+	}
 }
 
 func TestSendEmail(t *testing.T) {
-	client, _, emailTemplateId := setup(t)
+	client, _, emailTemplateID := setup(t)
 
 	emailAddress := os.Getenv("EMAIL_RECIPIENT")
 	if emailAddress == "" {
@@ -75,7 +79,7 @@ func TestSendEmail(t *testing.T) {
 
 	ref := "TestSendEmail"
 	resp, err := client.SendEmail(
-		emailTemplateId,
+		emailTemplateID,
 		emailAddress,
 		notify.Personalisation{
 			{"name", "John"},
@@ -84,7 +88,7 @@ func TestSendEmail(t *testing.T) {
 		notify.Reference(ref),
 	)
 	if err != nil {
-		t.Fatalf("Error sending email: %v", err)
+		t.Errorf("Error sending email: %v", err)
 	}
 
 	if resp.ID == "" {
@@ -93,65 +97,86 @@ func TestSendEmail(t *testing.T) {
 	if resp.URI == "" {
 		t.Errorf("Response URI should not be empty")
 	}
-	assertEqual(t, "Hi John,\n\nMy favourite colour is pink.\n", resp.Content.Body)
+	wantedBody := "Hi John,\n\nMy favourite colour is pink.\n"
+	if resp.Content.Body != wantedBody {
+		t.Errorf("Got %v, want %v", resp.Content.Body, wantedBody)
+	}
 }
 
 func TestGetAllTemplates(t *testing.T) {
-	client, smsTemplateId, emailTemplateId := setup(t)
+	client, smsTemplateID, emailTemplateID := setup(t)
 	resp, err := client.GetAllTemplates("")
 	if err != nil {
-		t.Fatalf("Error fetching all templates: %v", err)
+		t.Errorf("Error fetching all templates: %v", err)
 	}
 
-	assertTemplateFound(t, resp, []string{emailTemplateId, smsTemplateId})
+	assertTemplateFound(t, resp, []string{emailTemplateID, smsTemplateID})
 }
 
 func TestGetSMSTemplates(t *testing.T) {
-	client, smsTemplateId, _ := setup(t)
+	client, smsTemplateID, _ := setup(t)
 	resp, err := client.GetAllTemplates("sms")
 	if err != nil {
 		t.Fatalf("Error fetching sms templates: %v", err)
 	}
 
-	assertTemplateFound(t, resp, []string{smsTemplateId})
+	assertTemplateFound(t, resp, []string{smsTemplateID})
 }
 
 func TestGetEmailTemplates(t *testing.T) {
-	client, _, emailTemplateId := setup(t)
+	client, _, emailTemplateID := setup(t)
 	resp, err := client.GetAllTemplates("email")
 	if err != nil {
 		t.Fatalf("Error fetching email templates: %v", err)
 	}
 
-	assertTemplateFound(t, resp, []string{emailTemplateId})
+	assertTemplateFound(t, resp, []string{emailTemplateID})
 }
 
-func TestGetTemplateById(t *testing.T) {
-	client, smsTemplateId, _ := setup(t)
-	resp, err := client.GetTemplateByID(smsTemplateId)
+func TestGetTemplateByID(t *testing.T) {
+	client, smsTemplateID, _ := setup(t)
+	resp, err := client.GetTemplateByID(smsTemplateID)
 	if err != nil {
 		t.Fatalf("Error fetching template by id: %v", err)
 	}
-	assertEqual(t, "go-sdk-test-sms", resp.Name)
-	assertEqual(t, "sms", resp.Type)
+	wantedName := "go-sdk-test-sms"
+	if resp.Name != wantedName {
+		t.Errorf("Got %v, want %v", resp.Name, wantedName)
+	}
+	wantedType := "sms"
+	if resp.Type != wantedType {
+		t.Errorf("Got %v, want %v", resp.Type, wantedType)
+	}
 }
 
-func TestGetTemplateByIdAndVersion(t *testing.T) {
-	client, _, emailTemplateId := setup(t)
-	resp, err := client.GetTemplateByIDAndVersion(emailTemplateId, 1)
+func TestGetTemplateByIDAndVersion(t *testing.T) {
+	client, _, emailTemplateID := setup(t)
+	resp, err := client.GetTemplateByIDAndVersion(emailTemplateID, 1)
 	if err != nil {
 		t.Fatalf("Error fetching template version: %v", err)
 	}
 
-	assertEqual(t, "go-sdk-test-email", resp.Name)
-	assertEqual(t, "email", resp.Type)
-	assertEqual(t, 1, resp.Version)
-	assertEqual(t, "Hi ((name)),\r\n\r\nMy favourite colour is ((colour)).", resp.Body)
+	wantedName := "go-sdk-test-email"
+	if resp.Name != wantedName {
+		t.Errorf("Got %v, want %v", resp.Name, wantedName)
+	}
+	wantedType := "email"
+	if resp.Type != wantedType {
+		t.Errorf("Got %v, want %v", resp.Type, wantedType)
+	}
+	wantedVersion := 1
+	if resp.Version != wantedVersion {
+		t.Errorf("Got %v, want %v", resp.Version, wantedVersion)
+	}
+	wantedBody := "Hi ((name)),\r\n\r\nMy favourite colour is ((colour))."
+	if resp.Body != wantedBody {
+		t.Errorf("Got %v, want %v", resp.Body, wantedBody)
+	}
 }
 
 func TestGenerateTemplatePreview(t *testing.T) {
-	client, _, emailTemplateId := setup(t)
-	resp, err := client.GenerateTemplatePreview(emailTemplateId, notify.Personalisation{
+	client, _, emailTemplateID := setup(t)
+	resp, err := client.GenerateTemplatePreview(emailTemplateID, notify.Personalisation{
 		{"name", "KD"},
 		{"colour", "yellow"},
 	})
@@ -159,18 +184,24 @@ func TestGenerateTemplatePreview(t *testing.T) {
 		t.Fatalf("Error fetching email templates: %v", err)
 	}
 
-	assertEqual(t, "Hi KD,\n\nMy favourite colour is yellow.\n", resp.Body)
-	assertEqual(t, "email", resp.Type)
-}
-
-func assertEqual(t *testing.T, expected, actual interface{}) {
-	if expected != actual {
-		t.Errorf("Expected %v but received %v", expected, actual)
+	wantedBody := "Hi KD,\n\nMy favourite colour is yellow.\n"
+	if resp.Body != wantedBody {
+		t.Errorf("Got %v, want %v", resp.Body, wantedBody)
+	}
+	wantedType := "email"
+	if resp.Type != wantedType {
+		t.Errorf("Got %v, want %v", resp.Type, wantedType)
 	}
 }
 
-func assertTemplateFound(t *testing.T, templates notify.Templates, templateIds []string) {
-	for _, id := range templateIds {
+// func assertEqual(t *testing.T, got, want interface{}) {
+// 	if got != want {
+// 		t.Errorf("Got %v, want %v", got, want)
+// 	}
+// }
+
+func assertTemplateFound(t *testing.T, templates notify.Templates, templateIDs []string) {
+	for _, id := range templateIDs {
 		found := false
 		for _, template := range templates {
 			if template.ID == id {
--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/govau/notify-client-go
 go 1.12
 
 require (
-	github.com/stretchr/testify v1.3.0
 	golang.org/x/crypto v0.0.0-20190411191339-88737f569e3a // indirect
 	gopkg.in/square/go-jose.v2 v2.3.1
 )
